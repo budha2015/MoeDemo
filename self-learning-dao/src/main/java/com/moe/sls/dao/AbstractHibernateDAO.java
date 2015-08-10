9cc4aa39ce37b8ef33f0821d6fc021bb8c91c3c8
sebin-p <sebin-p@DS-7071BCC4311A.HCLT.CORP.HCL.IN>
2015-08-10 12:45:16 +0530

    test

--- self-learning-dao/src/main/java/com/moe/sls/dao/AbstractHibernateDAO.java
+++ self-learning-dao/src/main/java/com/moe/sls/dao/AbstractHibernateDAO.java
@@ -2,7 +2,7 @@ package com.moe.sls.dao;
 
 public abstract class  AbstractHibernateDAO {
 
-	private SessionFactory sessionFactory ; 
+	private SessionFactory123 sessionFactory ; 
 	private static ServiceRegistry serviceRegistry;
 	private Session session;
 
